8ac90142d86f1ec472380ef001fda53f5c7328fc
Atul Kumar <111857956+atul812@users.noreply.github.com>
2025-01-19 11:31:22 +0000

    new change

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
-console.log("How are you?");
-const num1 = 50;
-const num2 = 100;
-let sum = num1 + num2;
-console.log(sum);
-// This is a small change
+// console.log("How are you?");
+// const num1 = 50;
+// const num2 = 100;
+// let sum = num1 + num2;
+// console.log(sum);
+// // This is a small change
+
+let num = Math.random();
+if  (num > 0){
+    console.log("It's a positive number");
+}
+else{
+    console.log("It's a negative number");
+}
